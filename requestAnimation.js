31149a6cf40c9ee751fbdc257490230a4bac3e42
Daniel McCoy <danieljmccoy85@gmail.com>
2018-04-23 13:57:36 -0700

    playing around with requestAnimationFrame to learn how to make some cool things rather than using suboptimal setInteraval or setTimeout

---
+++ requestAnimation.js
@@ -0,0 +1,55 @@
+ // example one
+var box = document.getElementById('box'),
+boxPos = 10,
+boxVelocity = 2,
+limit = 300,
+lastFrameTimeMs = 0,
+maxFPS = 60;
+
+function update() {
+boxPos += boxVelocity;
+// Switch directions if we go too far
+if (boxPos >= limit || boxPos <= 0) boxVelocity = -boxVelocity;
+}
+
+function draw() {
+box.style.left = boxPos + 'px';
+}
+
+function mainLoop(timestamp) {
+  if (timestamp < lastFrameTimeMs + (1000 / maxFPS)) {
+    requestAnimationFrame(mainLoop);
+    return;
+}
+lastFrameTimeMs = timestamp;
+
+update();
+draw();
+requestAnimationFrame(mainLoop);
+}
+
+requestAnimationFrame(mainLoop);
+
+
+
+// example two
+let globalID;
+let body = document.querySelector("body");
+let stop = document.querySelector("#stop");
+let start = document.querySelector("#start");
+
+function repeatOften() {
+  let createdDiv = document.createElement("div")
+  body.appendChild(createdDiv);
+  globalID = requestAnimationFrame(repeatOften);
+}
+
+globalID = requestAnimationFrame(repeatOften);
+
+stop.addEventListener("click", function(){
+    cancelAnimationFrame(globalID);
+})
+
+start.addEventListener("click", function(){
+  globalID = requestAnimationFrame(repeatOften);
+});
